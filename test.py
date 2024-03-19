14ba8da043f58f63c6439bca966c5b7037e64a02
Bryce <bagr247@g.uky.edu>
2024-03-19 23:43:53 +0100

    Error checking for angle orientation

--- test.py
+++ test.py
@@ -81,8 +81,8 @@ plt.show()
 
 # Example: Testing with new inputs
 
-new_yolo_x = 2.26
-new_yolo_y = 0.69
+new_yolo_x = 2.8674
+new_yolo_y = 1.3033
 if new_yolo_x >= 1.5:
     if new_yolo_y >= 0:
         coeff_x = coeff_x_cw
@@ -100,3 +100,63 @@ result_robot_y = yolo_to_robot_y(new_yolo_y, coeff_y)
 
 print("For YOLO x = {:f}, Robot x = {:f}".format(new_yolo_x, result_robot_x))
 print("For YOLO y = {:f}, Robot y = {:f}".format(new_yolo_y, result_robot_y))
+
+# Define the function to calculate errors for a given YOLO coordinate
+def calculate_error(yolo_x, yolo_y, cw_points, ccw_points):
+    if yolo_x >= 1.5:
+        ground_truth_robot_x = np.interp(yolo_x, cw_points['yolo_x'], cw_points['robot_x'])
+        ground_truth_robot_y = np.interp(yolo_y, cw_points['yolo_y'], cw_points['robot_y'])
+    else:
+        ground_truth_robot_x = np.interp(yolo_x, ccw_points['yolo_x'], ccw_points['robot_x'])
+        ground_truth_robot_y = np.interp(yolo_y, ccw_points['yolo_y'], ccw_points['robot_y'])
+
+    if yolo_x >= 1.5:
+        if yolo_y >= 0:
+            coeff_x = coeff_x_cw
+            coeff_y = coeff_y_cw
+        else:
+            coeff_x = coeff_x_ccw
+            coeff_y = coeff_y_ccw
+    else:
+        coeff_x = coeff_x_ccw
+        coeff_y = coeff_y_ccw
+
+    # Using the defined functions to calculate robot coordinates
+    result_robot_x = yolo_to_robot_x(yolo_x, coeff_x)
+    result_robot_y = yolo_to_robot_y(yolo_y, coeff_y)
+
+    # Calculate the error for robot x coordinate
+    error_robot_x = abs(result_robot_x - ground_truth_robot_x) / abs(ground_truth_robot_x)
+
+    # Calculate the error for robot y coordinate
+    error_robot_y = abs(result_robot_y - ground_truth_robot_y) / abs(ground_truth_robot_y)
+
+    return error_robot_x, error_robot_y
+
+# Define the range of YOLO coordinates to test along the plotted line
+test_yolo_x_range = np.linspace(0, 3.14, 100)
+test_yolo_y_range = np.linspace(-1.57, 1.57, 100)
+
+# Initialize lists to store errors for robot x and y coordinates
+errors_robot_x = []
+errors_robot_y = []
+
+# Iterate over each YOLO coordinate in the range
+for test_yolo_x, test_yolo_y in zip(test_yolo_x_range, test_yolo_y_range):
+    # Calculate the error for the given YOLO coordinates
+    error_robot_x, error_robot_y = calculate_error(test_yolo_x, test_yolo_y, cw_points, ccw_points)
+    
+    # Append the errors to the respective lists
+    errors_robot_x.append(error_robot_x)
+    errors_robot_y.append(error_robot_y)
+
+# Plot the errors along the entire plotted line
+plt.figure(figsize=(10, 6))
+plt.plot(test_yolo_x_range, errors_robot_x, label='Error for Robot X Coordinate')
+plt.plot(test_yolo_x_range, errors_robot_y, label='Error for Robot Y Coordinate')
+plt.title('Error Along Plotted Line')
+plt.xlabel('YOLO x Coordinate')
+plt.ylabel('Relative Error')
+plt.legend()
+plt.grid(True)
+plt.show()
